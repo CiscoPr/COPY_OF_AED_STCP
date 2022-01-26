cbcf1420d1b0612cea3232b626c9b30d737c9cb1
CiscoPr <fmamp24@gmail.com>
2022-01-26 17:16:20 +0000

    created new class for global graph

---
+++ global_graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by francisco on 26/01/22.
+//
+
+#include "global_graph.h"
---
+++ global_graph.h
@@ -0,0 +1,14 @@
+//
+// Created by francisco on 26/01/22.
+//
+
+#ifndef STCP_AED_PROJECT_GLOBAL_GRAPH_H
+#define STCP_AED_PROJECT_GLOBAL_GRAPH_H
+
+
+class global_graph {
+
+};
+
+
+#endif //STCP_AED_PROJECT_GLOBAL_GRAPH_H
